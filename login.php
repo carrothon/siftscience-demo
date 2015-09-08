9480ad06fd58898de8809bd369eacd585fff439f
zzy <zzy@geetest.com>
2015-09-08 17:57:13 +0800

    Init commit

---
+++ login.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="/css/main.css">
+	<script type="text/javascript">
+	var _user_id = 'al_capone'; // IMPORTANT! Set to the user's ID, username, or email address, or '' if not yet known.
+	var _session_id = 'unique_session_id'; // Set to a unique session ID for the visitor's current browsing session.
+
+	var _sift = window._sift = window._sift || [];
+	_sift.push(['_setAccount', '5511332fe4b03090e13099e8']);
+	_sift.push(['_setUserId', _user_id]);
+	_sift.push(['_setSessionId', _session_id]);
+	_sift.push(['_trackPageview']);
+	(function() {
+	  function ls() {
+	    var e = document.createElement('script');
+	    e.type = 'text/javascript';
+	    e.async = true;
+	    e.src = ('https:' === document.location.protocol ? 'https://' : 'http://') + 'cdn.siftscience.com/s.js';
+	    var s = document.getElementsByTagName('script')[0];
+	    s.parentNode.insertBefore(e, s);
+	  }
+	  if (window.attachEvent) {
+	    window.attachEvent('onload', ls);
+	  } else {
+	    window.addEventListener('load', ls, false);
+	  }
+	})();
+	</script>	
+	</head>
+	<body>
+		<div class="container">
+			<h1>测试 SiftScience 页面</h1>
+		</div>
+	</body>
+</html>

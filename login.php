eb1daa3053fe44f34413bae0d4ef0e60492d01ff
zzy <zzy@geetest.com>
2015-09-09 14:56:06 +0800

    Change the account

--- login.php
+++ login.php
@@ -1,12 +1,13 @@
 <html>
 	<head>
+		<title>Carrothon 开发者网站</title>
 		<link rel="stylesheet" href="/css/main.css">
 	<script type="text/javascript">
-	var _user_id = 'al_capone'; // IMPORTANT! Set to the user's ID, username, or email address, or '' if not yet known.
-	var _session_id = 'unique_session_id'; // Set to a unique session ID for the visitor's current browsing session.
+	var _user_id = 'asdfjkl@qq.com'; // IMPORTANT! Set to the user's ID, username, or email address, or '' if not yet known.
+	var _session_id = '529ca8050a00180790cf88b63468826a'; // Set to a unique session ID for the visitor's current browsing session.
 
 	var _sift = window._sift = window._sift || [];
-	_sift.push(['_setAccount', '5511332fe4b03090e13099e8']);
+	_sift.push(['_setAccount', '55d57364e4b0d743f2044371']);
 	_sift.push(['_setUserId', _user_id]);
 	_sift.push(['_setSessionId', _session_id]);
 	_sift.push(['_trackPageview']);
@@ -30,6 +31,11 @@
 	<body>
 		<div class="container">
 			<h1>测试 SiftScience 页面</h1>
+			<form action="">
+				<div><input type="text" name="name" id=""></div>
+				<div><input type="password" name="password" id=""></div>
+				<div><input type="submit" value="Sumbit"></div>
+			</form>
 		</div>
 	</body>
 </html>
